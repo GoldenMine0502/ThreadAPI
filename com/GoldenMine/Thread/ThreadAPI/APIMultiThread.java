cd5785190dd6da3f744889c89a07617fd60fb725
GoldenMine0502 <ehe123@naver.com>
2018-02-21 14:02:02 +0900

    stable version
    stable api multi thread updated / start() method added

--- com/GoldenMine/Thread/ThreadAPI/APIMultiThread.java
+++ com/GoldenMine/Thread/ThreadAPI/APIMultiThread.java
@@ -6,11 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class APIMultiThread implements APIThreadable {
-    private final long start = System.currentTimeMillis();
-
-    private double fps;
-
-    private int time;
     private List<APIThread> threads = new ArrayList<APIThread>();
 
     public APIMultiThread(TimeUnitFactory factory, double unit, int time) {
@@ -18,100 +13,72 @@ public abstract class APIMultiThread implements APIThreadable {
     }
 
     public APIMultiThread(final double fps, final int time) {
-        Delay delay = new Delay(fps*time);
+        /*create threads*/
+        for (int i = 0; i < time; i++) {
 
-        this.fps = fps;
-        this.time = time;
-
-        int keepup = -1000;
-
-        new Thread() {
-            public void run() {
-                try {
-                    long starts = start;
-                    //long starts = start;
-
-                    int lastremain = 0;
-
-                    for (int i = 0; i < time; i++) {
-
-                        APIThread t = new APIThread(fps, lastremain) {
-                            public void onThreadExecute() throws InterruptedException {
-                                process(); // APIMultiThread의 execute()메서드를 실행하고 싶다
-                            }
-
-                            @Override
-                            public void onKeepUp() {
-                                processKeepup();
-                            }
-
-                            @Override
-                            public void onInterrupt() {
-                                processIntterrupt();
-                            }
-
-                            @Override
-                            public void onStart() {
-                                processStart();
-                            }
-
-                            @Override
-                            public void onPause() {
-                                processPause();
-                            }
-
-                            @Override
-                            public void onResume() {
-                                processResume();
-                            }
-
-                            @Override
-                            public void onStop() {
-                                processStop();
-                            }
-                        };
-                        t.start();
-                        threads.add(t);
-
-                        int ms = delay.getRemainMS(starts);
-                        int msplus = delay.getUpdateMS();
-                        long cal = delay.keepUp(starts, keepup);
-
-                        if(cal>0) {
-                            onKeepUp();
-                            starts+=cal;
-                            continue;
-                        }
-                        starts+=delay.getMS() + msplus;
-
-                        Delay.sleep(ms + msplus);
-
-                        lastremain = ms + msplus;
-                    }
-                } catch(InterruptedException ex) {
-                    //-onInterrupt();
+            APIThread t = new APIThread(fps, (int) Math.round(fps / time * 1000 * i)) {
+                public void onThreadExecute() throws InterruptedException {
+                    process();
                 }
-            }
-        }.start();
+
+                @Override
+                public void onKeepUp() {
+                    processKeepup();
+                }
+
+                @Override
+                public void onInterrupt() {
+                    processIntterrupt();
+                }
+
+                @Override
+                public void onStart() {
+                    processStart();
+                }
+
+                @Override
+                public void onPause() {
+                    processPause();
+                }
+
+                @Override
+                public void onResume() {
+                    processResume();
+                }
+
+                @Override
+                public void onStop() {
+                    processStop();
+                }
+            };
+            threads.add(t);
+        }
     }
+
     @Override
     public void APIPause() {
-        for(int i = 0; i < threads.size(); i++) {
+        for (int i = 0; i < threads.size(); i++) {
             threads.get(i).APIPause();
         }
     }
 
     @Override
     public void APIResume() {
-        for(int i = 0; i < threads.size(); i++) {
+        for (int i = 0; i < threads.size(); i++) {
             threads.get(i).APIResume();
         }
     }
 
     @Override
     public void APIStop() {
-        for(int i = 0; i < threads.size(); i++) {
+        for (int i = 0; i < threads.size(); i++) {
             threads.get(i).APIStop();
+        }
+    }
+
+    public void start() {
+        for (int i = 0; i < threads.size(); i++) {
+            threads.get(i).start();
         }
     }
 
@@ -139,6 +106,8 @@ public abstract class APIMultiThread implements APIThreadable {
         onResume();
     }
 
-    private void processStop() { onStop(); }
+    private void processStop() {
+        onStop();
+    }
 
 }
